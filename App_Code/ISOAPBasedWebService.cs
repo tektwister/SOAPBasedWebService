40c0d7a00dc804c9d26b0143f3235ac451560f34
Aravind Raj <aravinddevaraj777@gmail.com>
2019-05-11 14:35:59 +0530

    Initial commit

---
+++ App_Code/ISOAPBasedWebService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface ISOAPBasedWebService
+{
+
+
+	// TODO: Add your service operations here
+    // returns a welcome message
+    [OperationContract]
+    string Welcome(string yourName);
+
+    [OperationContract]
+    int Add(int val1, int val2);
+
+}
+
---
+++ App_Code/SOAPBasedWebService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class SOAPBasedWebService : ISOAPBasedWebService
+{
+    // returns a welcome message
+    public string Welcome(string yourName)
+    {
+       
+        return string.Format(
+           "Welcome to WCF Web Services with SOAP and XML, {0}!",
+           yourName);
+    } // end method Welcome
+    public int Add(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+
+}
